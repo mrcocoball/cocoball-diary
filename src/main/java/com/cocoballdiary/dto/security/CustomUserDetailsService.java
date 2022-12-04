16636b284d00edb2f85ae09dc4148ed5b2f453aa
mrcocoball <gunmaru93@gmail.com>
2022-12-04 21:14:23 +0900

    #12 - SecurityConfig 설정
    * SecurityConfig 설정과 관련된 클래스를 처리
    (UserDetailsService, 403 Exception Handler 등)

---
+++ src/main/java/com/cocoballdiary/dto/security/CustomUserDetailsService.java
@@ -0,0 +1,48 @@
+package com.cocoballdiary.dto.security;
+
+import com.cocoballdiary.domain.User;
+import com.cocoballdiary.dto.security.dto.UserSecurityDto;
+import com.cocoballdiary.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<User> result = userRepository.getWithRoles(username);
+
+        if (result.isEmpty()) {
+            throw new UsernameNotFoundException("유저를 찾을 수 없습니다");
+        }
+
+        User user = result.get();
+
+        UserSecurityDto userSecurityDto =
+                new UserSecurityDto(
+                        user.getUid(),
+                        user.getPassword(),
+                        user.getEmail(),
+                        false,
+                        user.isDeleted(),
+                        user.getRoleSet()
+                                .stream().map(userRole -> new SimpleGrantedAuthority("ROLE_" + userRole.name()))
+                                .collect(Collectors.toList())
+                );
+
+        return userSecurityDto;
+    }
+
+}
