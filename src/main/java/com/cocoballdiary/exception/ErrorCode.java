756d48f89b60885b86e2db90709cd0933cdc8549
mrcocoball <gunmaru93@gmail.com>
2022-12-04 21:24:42 +0900

    #12 - 기타 수정

--- src/main/java/com/cocoballdiary/exception/ErrorCode.java
+++ src/main/java/com/cocoballdiary/exception/ErrorCode.java
@@ -19,11 +19,10 @@ public enum ErrorCode {
 
     // 404 NOT_FOUND : Resource 를 찾을 수 없음
     ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다."),
-    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "덧글을 찾을 수 없습니다.");
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "덧글을 찾을 수 없습니다."),
 
     // 409 CONFICT : Resource의 현재 상태와 충돌, 보통 중복된 데이터 존재
-    // AGENT_EXISTS(HttpStatus.CONFLICT, "그룹 내 에이전트가 존재하여 삭제 할 수 없습니다."),
-    // CLIENT_EXISTS(HttpStatus.CONFLICT, "매핑된 광고주가 존재하여 삭제할 수 없습니다."),
+    USER_EXISTS(HttpStatus.CONFLICT, "중복된 유저가 있습니다.");
 
     // 500 INTERNAL_SERVER_ERROR : 서버 내부 오류, 발견되지 않은 비즈니스 로직 상 문제나 Runtime Exception
     // INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.");
