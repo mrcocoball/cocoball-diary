38b04d353cecf9ce04b593eb0b631d91788de8dc
mrcocoball <gunmaru93@gmail.com>
2022-12-05 00:17:30 +0900

    #15 - RestControllerAdvice 수정
    * CustomException 외에 Data I/O 관련 Exception, FK 관련 Exception 처리 기능 추가

--- src/main/java/com/cocoballdiary/exception/CustomRestAdvice.java
+++ src/main/java/com/cocoballdiary/exception/CustomRestAdvice.java
@@ -1,18 +1,23 @@
 package com.cocoballdiary.exception;
 
+import com.cocoballdiary.dto.response.ErrorResponse;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
-@RestController
+@RestControllerAdvice
 @Log4j2
 public class CustomRestAdvice {
 
@@ -36,9 +41,9 @@ public class CustomRestAdvice {
         return ResponseEntity.badRequest().body(errorMap);
     }
 
-    @ExceptionHandler(DiaryException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<Map<String, String>> handleNotFoundException(DiaryException e) {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+    public ResponseEntity<Map<String, String>> handleForeignKeyException(Exception e) {
 
         log.error(e);
 
@@ -46,5 +51,52 @@ public class CustomRestAdvice {
 
         return ResponseEntity.badRequest().body(errorMap);
     }
+
+    @ExceptionHandler({
+            NoSuchElementException.class,
+            EmptyResultDataAccessException.class }) //추가
+    @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+    public ResponseEntity<Map<String, String>> handleNoSuchElement(Exception e) {
+
+        log.error(e);
+
+        Map<String, String> errorMap = new HashMap<>();
+
+        errorMap.put("time", ""+System.currentTimeMillis());
+        errorMap.put("msg",  "No Such Element Exception");
+        return ResponseEntity.badRequest().body(errorMap);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Map<String, String>> handleEntityNotFoundException(Exception e) {
+
+        log.error(e);
+
+        Map<String, String> errorMap = new HashMap<>();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMap);
+    }
+
+    @ExceptionHandler(DiaryException.class)
+    public ResponseEntity<ErrorResponse> handleDiaryException(DiaryException e) {
+
+        log.error(e);
+
+        return ErrorResponse.toResponseEntity(e.getErrorCode());
+    }
+
+    /*
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, String>> handleGeneralException(Exception e) {
+
+        log.error(e);
+
+        Map<String, String> errorMap = new HashMap<>();
+
+        return ResponseEntity.badRequest().body(errorMap);
+    }
+    */
 
 }
