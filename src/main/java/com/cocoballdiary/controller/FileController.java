b0075a1b0a44f9712cb7a35eb37b3d79fc1971c3
mrcocoball <gunmaru93@gmail.com>
2022-12-14 01:28:54 +0900

    #32 - 파일 컨트롤러 추가, 업로드 js 추가

---
+++ src/main/java/com/cocoballdiary/controller/FileController.java
@@ -0,0 +1,131 @@
+package com.cocoballdiary.controller;
+
+import com.cocoballdiary.dto.UploadFileDto;
+import com.cocoballdiary.dto.UploadResultDto;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+@RestController
+@Log4j2
+public class FileController {
+
+    @Value("${com.cocoballdiary.upload.path}")
+    private String uploadPath;
+
+    @Operation(summary = "Upload POST", description = "[POST] 파일 등록")
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public List<UploadResultDto> upload(UploadFileDto uploadFileDto) {
+
+        log.info(uploadFileDto);
+
+        if (uploadFileDto.getFiles() != null) {
+
+            final List<UploadResultDto> list = new ArrayList<>();
+
+            uploadFileDto.getFiles().forEach(multipartFile -> {
+
+                String originalName = multipartFile.getOriginalFilename();
+                log.info(originalName);
+
+                String uuid = UUID.randomUUID().toString();
+
+                Path savePath = Paths.get(uploadPath, uuid + "_" + originalName);
+
+                boolean image = false;
+
+
+                // 실제 저장 작업
+
+                try {
+                    multipartFile.transferTo(savePath);
+
+                    //이미지 파일의 종류라면
+                    if (Files.probeContentType(savePath).startsWith("image")) {
+
+                        image = true;
+
+                        File thumbFile = new File(uploadPath, "s_" + uuid + "_" + originalName);
+
+                        Thumbnailator.createThumbnail(savePath.toFile(), thumbFile, 200, 200);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                list.add(UploadResultDto.builder()
+                        .uuid(uuid)
+                        .fileName(originalName)
+                        .img(image).build()
+                );
+
+            });
+
+            return list;
+        }
+
+        return null;
+    }
+
+
+    @Operation(summary = "File GET", description = "[GET] 첨부 파일 조회")
+    @GetMapping("/view/{fileName}")
+    public ResponseEntity<Resource> viewFileGET(@PathVariable String fileName) {
+
+        Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+        String resourceName = resource.getFilename();
+        HttpHeaders headers = new HttpHeaders();
+
+        try {
+            headers.add("Content-Type", Files.probeContentType(resource.getFile().toPath()));
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+        return ResponseEntity.ok().headers(headers).body(resource);
+    }
+
+    @Operation(summary = "File DELETE", description = "[DELETE] 파일 삭제")
+    @DeleteMapping("/remove/{fileName}")
+    public Map<String, Boolean> removeFile(@PathVariable String fileName) {
+
+        Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+        String resourceName = resource.getFilename();
+
+        Map<String, Boolean> resultMap = new HashMap<>();
+        boolean removed = false;
+
+        try {
+            String contentType = Files.probeContentType(resource.getFile().toPath());
+            removed = resource.getFile().delete();
+
+            //섬네일이 존재한다면
+            if (contentType.startsWith("image")) {
+                File thumbnailFile = new File(uploadPath + File.separator + "s_" + fileName);
+                thumbnailFile.delete();
+            }
+
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+
+        resultMap.put("result", removed);
+
+        return resultMap;
+    }
+
+}
