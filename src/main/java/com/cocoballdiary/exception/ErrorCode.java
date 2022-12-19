2e3535e54d775cebba60d6de78df39f5c9744d47
mrcocoball <gunmaru93@gmail.com>
2022-12-19 21:28:47 +0900

    #43 - 에러 페이지 / 컨트롤러

--- src/main/java/com/cocoballdiary/exception/ErrorCode.java
+++ src/main/java/com/cocoballdiary/exception/ErrorCode.java
@@ -14,7 +14,7 @@ public enum ErrorCode {
     OK(HttpStatus.OK, "ok"),
 
     // 400 BAD_REQUEST : 잘못된 요청
-    // BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
 
 
     // 404 NOT_FOUND : Resource 를 찾을 수 없음
@@ -23,10 +23,10 @@ public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
 
     // 409 CONFICT : Resource의 현재 상태와 충돌, 보통 중복된 데이터 존재
-    USER_EXISTS(HttpStatus.CONFLICT, "중복된 유저가 있습니다.");
+    USER_EXISTS(HttpStatus.CONFLICT, "중복된 유저가 있습니다."),
 
     // 500 INTERNAL_SERVER_ERROR : 서버 내부 오류, 발견되지 않은 비즈니스 로직 상 문제나 Runtime Exception
-    // INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.");
 
     public String getMessage(Throwable e) {
         return this.getMessage(this.getMessage() + " - " + e.getMessage());
